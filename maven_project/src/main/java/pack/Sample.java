93829d7cc9a926a1b3f149d3ac2d1d60225aae17
AMAL RAJ <AMAL RAJ@LAPTOP-92ICR69A>
2024-12-31 11:56:10 +0530

    initial commit

---
+++ maven_project/src/main/java/pack/Sample.java
@@ -0,0 +1,27 @@
+package pack;
+
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sample.addition();
+		Sample obj=new Sample();
+		obj.multiply();
+			}
+		public static void addition()
+		{
+			int a=5,b=6,sum;
+			sum=a+b;
+			System.out.println(sum);
+		}
+
+		public void multiply()
+		{
+		int c=6,d=2,result;
+		result=c*d;
+		System.out.println(result);
+		}
+	}
+
+
